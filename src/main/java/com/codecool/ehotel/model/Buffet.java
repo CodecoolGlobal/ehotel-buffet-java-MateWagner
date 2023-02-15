cfb07c6b08326ccd870394a2abb4f7d819deeaee
MateWagner <wwagnermate@gmail.com>
2023-02-15 14:20:26 +0100

    feat: buffet and tests

--- src/main/java/com/codecool/ehotel/model/Buffet.java
+++ src/main/java/com/codecool/ehotel/model/Buffet.java
@@ -1,10 +1,17 @@
 package com.codecool.ehotel.model;
 
-import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public record Buffet(List<FoodItem> foodItems) {
+public class Buffet {
+    private List<FoodItem> foodItems;
+    Buffet(){
+        foodItems = new ArrayList<>();
+    }
+    Buffet(List<FoodItem> foodItems){
+        this.foodItems = foodItems;
+    }
     public void addMany(List<FoodItem> foodItems) {
         this.foodItems.addAll(foodItems);
     }
@@ -12,21 +19,25 @@ public record Buffet(List<FoodItem> foodItems) {
     public void removeMany(List<FoodItem> foodItems) {
         this.foodItems.removeAll(foodItems);
     }
-    public void removeOne(FoodItem meal){
-        foodItems.remove(meal);
+    public void removeOne(FoodItem item){
+        foodItems.remove(item);
     }
     public Optional<FoodItem> getFreshestMeal(FoodType foodType){
         return foodItems.stream().filter(item -> item.getType().equals(foodType))
                  .sorted((o1, o2) -> o1.getAgeCycle() - o2.getAgeCycle())
                  .findFirst();
     }
-    private void increaseAgePairItem() {
+    public void increaseAgePairItem() {
         foodItems.forEach(FoodItem::increaseAge);
     }
     public List<FoodItem> collectWaste(){
         return foodItems.stream().filter(FoodItem::isItemExpired).toList();
     }
 
-
-
+    public List<FoodItem> getFoodItems() {
+        return foodItems;
+    }
+    public List<FoodItem> dalyCleanUp(){
+        return foodItems.stream().filter(FoodItem::isDalyWaste).toList();
+    }
 }

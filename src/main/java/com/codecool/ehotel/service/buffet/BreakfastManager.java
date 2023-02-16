44d536a911044233aab4b1c0b83e0196a9199a32
MateWagner <wwagnermate@gmail.com>
2023-02-16 11:00:55 +0100

    feat: base logic in progress

--- src/main/java/com/codecool/ehotel/service/buffet/BreakfastManager.java
+++ src/main/java/com/codecool/ehotel/service/buffet/BreakfastManager.java
@@ -9,12 +9,14 @@ public class BreakfastManager implements BuffetService {
     Map<FoodItem, Integer> batch;
 
    // int cycle = 0;
-    public void serve(List<Guest> guests) {
+    public void serve(ArrayList<ArrayList<Guest>> guests) {
+        int maximumGuestNumber = guests.stream().mapToInt(ArrayList::size).sum();
+        int minimumBatchSizes = (int) (maximumGuestNumber * 0.5);
         //Refill the buffet
-        refill(batch, buffet);
+        refill();
         //Try to feed the guests
         for (Guest guest : guests)
-            if (!consumeFreshest(guest.guestType().getMealPreferences()))
+            if (!consumeFreshest(guest.getGuestType().getMealPreferences()))
                 // TODO: Set guest happiness to unhappy
                 System.out.println("Guest is unhappy!");  // <- This is just a placeholder!!!
         //Returns the cost of all wasted meals
@@ -29,7 +31,8 @@ public class BreakfastManager implements BuffetService {
         this.batch = new HashMap<>();
     }
 
-    public void refill(Map<FoodItem, Integer> batch, Buffet buffet) {
+    public void refill() {
+        createDummyBatches();
         List<FoodItem> newFoods = new ArrayList<>();
         for (Map.Entry<FoodItem, Integer> entry : batch.entrySet()) {
             for (int i = 0; i < entry.getValue(); i++)
@@ -40,7 +43,7 @@ public class BreakfastManager implements BuffetService {
     }
 
     @Override
-    public void createBatch(MealType mealType, int portion, int timeStamp) {
+    public void createBatch(MealType mealType, int portion) {
         batch.put(new FoodItem(0, mealType), portion);
     }
 
@@ -68,6 +71,12 @@ public class BreakfastManager implements BuffetService {
             i++;
         }
         return freshMeal;
+    }
+    private void createDummyBatches(){
+        int minAvailablePotion = 10; //todo Need to move upper
+        for (MealType mealType : MealType.values()) {
+           createBatch(mealType, minAvailablePotion - buffet.foodItemCount(mealType));
+        }
     }
 }
 
